2eade0a8ea4e217713cf3856876271be5b2e6da0
tintin007k <tintin007k@gmail.com>
2024-02-07 21:57:47 +0530

    nw(chapter 2,stateful handler): added new stateful handler for count

--- main.go
+++ main.go
@@ -6,21 +6,26 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
 	fmt.Println("Hello from web-server")
 	mux := http.NewServeMux()
-	serveImageFromAssets(mux)
+	apiCfg := apiConfig{fileserverHits: 0}
+	mux.Handle(
+		"/app/",
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))),
+	)
 	mux.Handle("/healthz", customHandler(mux))
+	mux.Handle("/metrics", apiCfg.metricsHandler(mux))
+	mux.Handle("/reset", apiCfg.metricsReset(mux))
 	corsMux := middlewareCors(mux)
 	srvErr := http.ListenAndServe(":8080", corsMux)
 	if srvErr != nil {
 		log.Fatal(srvErr)
 	}
-}
-
-func serveImageFromAssets(mux *http.ServeMux) {
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	//mux.Handle("/assets/", http.FileServer(http.Dir("./logo.png")))
 }
 
 func customHandler(handler http.Handler) http.Handler {
@@ -45,6 +50,7 @@ func middlewareCors(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*") //https://www.boot.dev
 		w.Header().Set("Access-Control-Allow-Methods", "GET")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Cache-Control", "no-cache")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
---
+++ middleware.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+
+	//log.Println("file hit value is", cfg.fileserverHits)
+	//fs := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+
+}
+
+func addHeaders(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "no-cache")
+		next.ServeHTTP(w, r)
+	}
+}
+
+func (cfg *apiConfig) metricsHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := "Hits: " + strconv.Itoa(cfg.fileserverHits)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		if r.Method == "GET" {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(res))
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsReset(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits = 0
+		if r.Method == "GET" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
